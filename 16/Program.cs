473cb09cf83182559e6b1a342e49b55d875db00b
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-07-14 09:20:38 +0700

    задача 16

---
+++ 16/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int a=14;
+bool b=a%7==0 && a%23==0;
+System.Console.WriteLine(b);
+if (b)
+{
+    System.Console.WriteLine("Кратно");
+}
+
+else
+{
+    System.Console.WriteLine("Не кратно");
+}
