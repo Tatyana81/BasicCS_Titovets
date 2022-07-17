65f768512e345c9444467bdf8259cea1e241e372
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-07-17 18:38:06 +0700

    добавила 25 задачу, которая недавно стала курсивом

---
+++ 25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+int a=5;
+int count=0;
+
+int Number (int a)
+{
+    return Convert.ToInt32(Math.Pow(a,3));
+}
+
+while (count<a)
+{
+    count++;
+    System.Console.Write(Number(count)+" ");
+}
+
+
