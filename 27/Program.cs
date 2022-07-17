95c12a061924c86d68d001471d51e4f0750a532f
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-07-17 18:59:53 +0700

    сделала 27 задачу

---
+++ 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int a=5,b=3;
+int count=1;
+
+int c=a;
+int Power (int a)
+{
+    return a*c;
+}
+
+while (count<b)
+{
+    a=Power(a);
+    count++;
+}
+
+System.Console.WriteLine(a);
