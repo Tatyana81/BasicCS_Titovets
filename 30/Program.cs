bc25f15e13b3ac9e60f55f5f6b27faa63b80f117
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-07-17 22:00:26 +0700

    добавила 30 задачу

---
+++ 30/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int N=4,p=1;
+int count=1;
+
+int Number (int N)
+{
+    return p=p*count;
+}
+
+while (count<N)
+{
+    count++;
+    Number(N);
+}
+System.Console.WriteLine(p);
+
