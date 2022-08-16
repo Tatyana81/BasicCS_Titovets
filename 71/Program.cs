569c150d91bc38346693bde67e891c387c653bec
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-08-16 16:52:45 +0700

    семинар 7

---
+++ 71/Program.cs
@@ -0,0 +1,22 @@
+﻿// функция Аккермана A(m,n)
+
+int recursion(int m, int n)
+    {
+        
+        if (m == 0)
+            {
+                return n + 1;
+            } 
+            else if (n == 0 && m > 0)
+                {
+                    return recursion(m - 1, 1);
+                } 
+                    else
+                    {
+                        return recursion(m - 1, recursion(m, n - 1));
+                    }
+    }
+    
+Console.Write(recursion(3, 1));
+               
+    
