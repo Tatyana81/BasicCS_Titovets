e815f6bf3364ac6f255dae7d23b27f905ad6d924
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-08-18 22:37:58 +0700

    семинар 8

---
+++ string3/Program.cs
@@ -0,0 +1,12 @@
+﻿// С клавиатуры вводится строка разделенная точкой. 
+//Подсчитать количество символов до точки
+
+
+string s=Console.ReadLine();
+char[] cc=s.ToArray();
+s=new String(cc);
+int count=0;
+for(int i=0;i<cc.Length;i++)
+    if (s[i]=='.') 
+        {System.Console.WriteLine(count);return;}
+        else count++;
---
+++ string5/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Определить являются ли введенные с клавиатуры символы правильной записью целого числа. 
+//Вычислить сумму цифр введенного числа
+
+string s=Console.ReadLine();
+char[] cc=s.ToArray();
+s=new String(cc);
+int count=0;
+for(int i=0;i<cc.Length;i++)
+    if (s[i]>='0'&&s[i]<='9') 
+        count=count+(int)(char.GetNumericValue(s[i]));
+        else 
+        {System.Console.WriteLine("не целое цисло");return;};
+System.Console.WriteLine(count);
+
