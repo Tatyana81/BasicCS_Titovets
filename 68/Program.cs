b8710a5d1e53213dc66f348fcddf4b4d193918e6
«TanchaTi» <tatyanamokrushina@yandex.ru>
2022-08-22 21:39:22 +0700

    семинар 9

---
+++ 68/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+// функция Аккермана A(m,n)
+
+int recursion(int m, int n)
+    {
+        
+        if (m == 0)
+            {
+                return n + 1;
+            } 
+            else if (n == 0 && m > 0)
+                {
+                    return recursion(m - 1, 1);
+                } 
+                    else
+                    {
+                        return recursion(m - 1, recursion(m, n - 1));
+                    }
+    }
+    
+Console.Write(recursion(2, 3));
